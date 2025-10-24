421eff600de2211fdfe5702239a93b03a390cfa1
Enzo <108096900+enmedinad@users.noreply.github.com>
2025-10-24 19:03:33 -0300

    Conection TEST
    Just testing

--- wp-n8n-connector.php
+++ wp-n8n-connector.php
@@ -5,3 +5,10 @@ Author: Enzo Medina.
 Version: 0.0.1a
 License: Private Use Only
 -->
+
+<?php
+// Prevent direct access to the file
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
+?>
